42714eec5e1ae33ff20dd7e4cece207f7856ee8f
wobin1 <nathanielmusa3@gmail.com>
2024-02-18 01:31:31 +0100

    added http and auth service code

--- src/app/shared/services/authentication/auth.service.ts
+++ src/app/shared/services/authentication/auth.service.ts
@@ -1,9 +1,119 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable, of } from 'rxjs';
+import { catchError, mapTo, tap } from 'rxjs/operators';
+
+const baseUrl = {
+  jwt_token: 'VT_TOKEN',
+  refresh_token: 'TP_REFRESH',
+  server: 'http://localhost:3000/',
+
+  refresh: 'token/refresh/',
+
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private readonly JWT_TOKEN = baseUrl.jwt_token;
+  private readonly REFRESH_TOKEN = baseUrl.refresh_token;
+  private helper = new JwtHelperService();
+  
+  private base_url = baseUrl.server;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  login(user: any): Observable<boolean> {
+    return this.http.post<any>(this.base_url + 'api/auth/login', user).pipe(
+      tap((res: any) => {
+        this.storeTokens({ token: res?.token });
+        localStorage.setItem('user', JSON.stringify(res.data))
+        localStorage.setItem('userId', res.data._id)
+        
+      }),
+      mapTo(true),
+      catchError((error: any) => {
+        return of(false);
+      })
+    );
+  }
+
+  register(user: any): Observable<boolean> {
+    return this.http.post<any>(this.base_url + 'api/auth/sign-up', user).pipe(
+      tap((tokens: any) => {
+        // this.storeTokens({ token: tokens?.data?.token });
+        console.log(tokens);
+        
+      }),
+      mapTo(true),
+      catchError((error: any) => {
+        return of(false);
+      })
+    );
+  }
+
+  // check if user is login
+  isLoggedIn() {
+    // this.checkExpired()
+    return !!this.getJwtToken();
+  }
+
+  checkExpired() {
+    const isExpired = this.helper.isTokenExpired(this.getJwtToken());
+    if (isExpired) {
+      this.logout();
+    }
+  }
+
+  // refresh token
+  refreshToken() {
+    const data = {
+      refresh: this.getRefreshToken(),
+    };
+    return this.http.post<any>(this.base_url + baseUrl.refresh, data).subscribe(
+      (tokens: any) => {
+        this.storeJwtToken(tokens.access);
+        return true;
+      },
+      () => {
+        return false;
+      }
+    );
+  }
+
+  getJwtToken(): any {
+    return localStorage.getItem(this.JWT_TOKEN);
+  }
+
+  // logout user
+  public logout() {
+    console.log('sss');
+    
+    this.removeTokens();
+    // this.router.navigate(['/auth/login/']);
+  }
+
+  getRefreshToken(): any {
+    return localStorage.getItem(this.REFRESH_TOKEN);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(this.JWT_TOKEN, jwt);
+  }
+
+  public storeTokens(tokens: any) {
+    localStorage.setItem(this.JWT_TOKEN, tokens?.token);
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(this.JWT_TOKEN);
+    localStorage.removeItem(this.REFRESH_TOKEN);
+  }
 }
--- src/app/shared/services/http-request/http.service.ts
+++ src/app/shared/services/http-request/http.service.ts
@@ -1,9 +1,97 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, map, of, retry, shareReplay } from 'rxjs';
+import { AuthService } from '../authentication/auth.service';
+
+const baseUrl = {
+  jwt_token: 'TP_TOKEN',
+  refresh_token: 'TP_REFRESH',
+  server: 'url',
+
+  refresh: 'token/refresh/',
+
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  private base_url = baseUrl.server;
+  private httpOptions = {
+    headers: {
+      Authorization: `Bearer ${this.authService.getJwtToken()}`,
+    },
+  };
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  getToken(){
+    this.httpOptions = {
+      headers: {
+        Authorization: `Bearer ${this.authService.getJwtToken()}`,
+      },
+    };
+  }
+
+  getSingleNoAuth(endpoint: any) {
+    return this.http.get(this.base_url + endpoint).pipe(retry(1), shareReplay());
+  }
+
+  getSingleNoAuthID(endpoint: any, id: any) {
+    return this.http.get(this.base_url + endpoint + id).pipe(retry(1));
+  }
+
+  postData(endpoint: any, data: any): Observable<any[]> {
+    return this.http
+      .post<any[]>(this.base_url + endpoint, data)
+  }
+
+  postAuthData(endpoint: any, data: any): Observable<any[]> {
+    this.getToken()
+    return this.http
+      .post<any[]>(this.base_url + endpoint, data, this.httpOptions)
+  }
+
+  getData(endpoint: any): Observable<any[]> {
+    return this.http
+      .get<any[]>(this.base_url + endpoint)
+      .pipe(retry(1), shareReplay());
+  }
+
+  getAuthData(endpoint: any): Observable<any[]> {
+    this.getToken()
+    return this.http
+      .get<any[]>(this.base_url + endpoint, this.httpOptions)
+      .pipe(retry(1), shareReplay());
+  }
+  
+  getAuthSingleID(endpoint: any, id: any): Observable<any[]> {
+    this.getToken()
+    return this.http
+      .get<any[]>(this.base_url + endpoint + id, this.httpOptions)
+      .pipe(retry(1), shareReplay());
+  }
+
+  updateData(endpoint: any, data: any): Observable<any[]> {
+    this.getToken()
+    return this.http
+      .patch<any[]>(this.base_url + endpoint, data, this.httpOptions)
+      .pipe(retry(1));
+  }
+
+  updatePutData(endpoint: any, data: any): Observable<any[]> {
+    this.getToken()
+    return this.http
+      .put<any[]>(this.base_url + endpoint, data, this.httpOptions)
+      .pipe(retry(1));
+  }
+
+  deleteData(endpoint: any, id: any): Observable<any[]> {
+    this.getToken()
+    return this.http.delete<any[]>(
+      this.base_url + endpoint + id,
+      this.httpOptions
+    );
+  }
 }
