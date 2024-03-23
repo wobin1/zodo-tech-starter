458e41339bceb4c8e234c7f5cd09d26feb4a3354
wobin1 <nathanielmusa3@gmail.com>
2024-03-23 16:11:50 +0100

    fixed lost component issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TestComponent } from './views/test/test.component';
 import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
 
   ],
   imports: [
